15a8e39f7b714896e33365230de662b23617a3dd
iOSBeginner <jenhausu@icloud.com>
2014-12-05 16:31:41 +0800

    初始化

---
+++ CoreData/CoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by 蘇健豪1 on 2014/12/5.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
