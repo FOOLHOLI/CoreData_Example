85949e736b51d050ef8e6ffab0970710087af6ed
iOSBeginner <jenhausu@icloud.com>
2014-12-05 16:49:54 +0800

    初始化

---
+++ CoreData/CoreData/ShowDataTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowDataTableViewController.h
+//  CoreData
+//
+//  Created by 蘇健豪1 on 2014/12/5.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowDataTableViewController : UITableViewController
+
+@end
