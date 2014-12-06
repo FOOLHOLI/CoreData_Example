8e0fb4ee157ab84ee29fe91359411efe7debf4ad
iOSBeginner <jenhausu@icloud.com>
2014-12-06 17:27:39 +0800

    初始化

---
+++ CoreData/CoreData/AddNewDataViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddNewDataViewController.h
+//  CoreData
+//
+//  Created by 蘇健豪1 on 2014/12/6.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddNewDataViewController : UIViewController
+
+@end
---
+++ CoreData/CoreData/AddNewDataViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddNewDataViewController.m
+//  CoreData
+//
+//  Created by 蘇健豪1 on 2014/12/6.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import "AddNewDataViewController.h"
+
+@interface AddNewDataViewController ()
+
+@end
+
+@implementation AddNewDataViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
